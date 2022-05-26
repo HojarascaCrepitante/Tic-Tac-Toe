1436f7c37728998c22e6128f37b154e04870125d
Lucas Vallejos <lucascolegioripari@gmail.com>
2022-05-26 14:51:18 -0300

    Rewrote the logic for the gameboard object, and added a display function, and a function to push content into the array, then returned the functions maintaining the array itself private

--- script.js
+++ script.js
@@ -1,6 +1,36 @@
-let gameBoard = (function(){
-   let board = {
-       board : [1,2,3,4,5,6,7,8,9]
+ 
+ let gameBoard = (function(){
+    let myBoard = [,,,
+                   ,,,
+                   ,,,];
+    
+    const pushToAray = function(arg){
+        myBoard.splice(0,1,arg)
+        console.log(myBoard)
     }
-   console.log(board);
-})()
+    
+   
+    const display = () =>{
+        for(let i = 0; i < myBoard.length; i++){
+               if(myBoard[i]) {
+                spot[i].textContent = myBoard[i];
+            }
+        }
+    }
+    return { pushToAray, display}
+}())
+
+const flowOfTheGame = function(){
+    let spot = document.querySelectorAll('.spot');
+
+}
+gameBoard.pushToAray("w")
+gameBoard.display()
+
+
+const playerObj = (name, legend) => {
+    let player = {
+       name,
+       legend,
+    }
+}
