09b241929165f004b0acfe95b8be125df38c6793
Lucas Vallejos <lucascolegioripari@gmail.com>
2022-06-06 16:58:08 -0300

    I completely refactored the code after days of struggling, so no idea how much it changed. Now my gameLogic and gameBoard modules are separate and the logic to create the players is in the gameLogic module, instead of getting the player symbol onclick now it runs inside the populateArray function.

--- script.js
+++ script.js
@@ -1,36 +1,64 @@
- 
- let gameBoard = (function(){
-    let myBoard = [,,,
-                   ,,,
-                   ,,,];
-    
-    const pushToAray = function(arg){
-        myBoard.splice(0,1,arg)
-        console.log(myBoard)
-    }
-    
-   
-    const display = () =>{
-        for(let i = 0; i < myBoard.length; i++){
-               if(myBoard[i]) {
-                spot[i].textContent = myBoard[i];
-            }
+const playerObj = function(symbol){
+     symbol
+    return {symbol}
+}
+const gameLogic = (function(){
+    let player1 = null;
+    let player2 = null;
+    let turn = 1;
+    let symbol = "";
+
+    let button1 = document.getElementById('player1')
+    button1.addEventListener('click', () => {
+        player1 =playerObj(button1.dataset.player)
+        console.log(player1)
+    })
+
+    let button2 = document.getElementById('player2')
+    button2.addEventListener('click', () =>{
+        player2 = playerObj(button2.dataset.player)
+        console.log(player2)
+    })
+
+    const getSymbol = function(){
+        if (turn == 1){
+            turn ++
+            return player1.symbol
+        }
+        if (turn == 2){
+            turn --
+            return player2.symbol
         }
     }
-    return { pushToAray, display}
-}())
+    return {getSymbol}
+})()
 
-const flowOfTheGame = function(){
-    let spot = document.querySelectorAll('.spot');
+const gameBoard = (function(){
+    let myBoard = [,,,,,,,,,];
+     console.log(myBoard)
+    let spots = document.querySelectorAll('.spot'); 
+    
+    
+    
+    let index = null;
 
-}
-gameBoard.pushToAray("w")
-gameBoard.display()
-
-
-const playerObj = (name, legend) => {
-    let player = {
-       name,
-       legend,
+    const findIndex = function(){
+         return index = this.dataset.index
+    }     
+    const populateArray = function(){
+        myBoard.splice(index ,1, gameLogic.getSymbol())
+        displayBoard()
+    }   
+    const displayBoard = function(){
+        for(let i = 0; i < myBoard.length; i++){
+            spots[i].textContent = myBoard[i]
+        }
+        console.log(myBoard)
     }
-}
+    spots.forEach(spot => spot.addEventListener('click', findIndex))
+    spots.forEach(spot => spot.addEventListener('click', populateArray))    
+    return {populateArray}
+})()
+
+
+
