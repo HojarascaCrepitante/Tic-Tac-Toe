db9f0981a5870603cca3771953dc3c0618c9395d
Lucas Vallejos <lucascolegioripari@gmail.com>
2022-06-09 17:39:32 -0300

    Final commit: I added a function to announce the winner and a button to restart the game, and wrote the conditions to check for a tie, it takes the totalMoves value and checks if there is a winner announced, if totalMoves equals to 9 and there is no winner it returns tie. i also changed the background color and the order of the form elements

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
-const playerObj = function(symbol){
-     symbol
-    return {symbol}
+const playerObj = function(name,symbol){
+    name 
+    symbol
+    return {name,symbol}
 }
 const gameLogic = (function(){
     let player1 = null;
     let player2 = null;
     let turn = 1;
-    let symbol = "";
+    
+    let congratulations = document.createElement('div')
+        congratulations.classList.add('congrats')
+    
+    let message = document.createElement('p')
+        message.setAttribute('id', 'message')
+    
+    let restartGame = document.createElement('button')
+        restartGame.textContent = "Restart"
+        restartGame.addEventListener('click', ()=>{
+            window.location.reload()
+        })
 
     let button1 = document.getElementById('player1')
     button1.addEventListener('click', () => {
-        player1 =playerObj(button1.dataset.player)
+        player1 =playerObj(document.getElementById('Player1').value ,button1.dataset.player)
         console.log(player1)
     })
 
     let button2 = document.getElementById('player2')
     button2.addEventListener('click', () =>{
-        player2 = playerObj(button2.dataset.player)
+        player2 = playerObj(document.getElementById('Player2').value ,button2.dataset.player)
         console.log(player2)
     })
 
     const getSymbol = function(){
-        if (turn == 1){
+        if (turn == 1 ){
             turn ++
             return player1.symbol
         }
@@ -30,34 +42,111 @@ const gameLogic = (function(){
             return player2.symbol
         }
     }
-    return {getSymbol}
+    const isWinner = function(){
+        gameBoard.winningBoard.call(player1.symbol, player1.name)
+        gameBoard.winningBoard.call(player2.symbol, player2.name )
+    }
+
+    const announceWinner = function(){
+        if(this == "Tie"){
+            message.textContent = "Tie"
+            congratulations.appendChild(message)
+            congratulations.appendChild(restartGame)
+            document.body.appendChild(congratulations)
+        }
+        if(this == player1.name){
+            message.textContent = ` "${player1.name}" wins`
+            congratulations.appendChild(message)
+            congratulations.appendChild(restartGame)
+            document.body.appendChild(congratulations)
+        }
+
+        if(this == player2.name){
+
+            message.textContent = ` "${player2.name}" wins`
+            congratulations.appendChild(message)
+            congratulations.appendChild(restartGame)
+            document.body.appendChild(congratulations)
+        }
+    }
+    
+        
+    return {getSymbol, isWinner,announceWinner}
 })()
 
 const gameBoard = (function(){
     let myBoard = [,,,,,,,,,];
      console.log(myBoard)
     let spots = document.querySelectorAll('.spot'); 
-    
-    
-    
     let index = null;
+    let totalMoves = 0;
+    let winningState = "";
 
     const findIndex = function(){
          return index = this.dataset.index
     }     
     const populateArray = function(){
+        if (!myBoard[index]){
         myBoard.splice(index ,1, gameLogic.getSymbol())
         displayBoard()
+        gameLogic.isWinner()
+        gameLogic.announceWinner.call(winningState)
+        }
     }   
     const displayBoard = function(){
         for(let i = 0; i < myBoard.length; i++){
             spots[i].textContent = myBoard[i]
         }
         console.log(myBoard)
+        totalMoves ++
     }
+    const winningBoard = function(winner){
+        console.log(this)
+        
+        if(myBoard[0] == this && myBoard[1] == this && myBoard[2] == this){
+            console.log(`${winner} wins`)
+            winningState = winner
+        }
+        if(myBoard[3] == this && myBoard[4] == this && myBoard[5] == this){
+            console.log(`${winner} wins`)
+            winningState = winner
+        }
+        if(myBoard[6] == this && myBoard[7] == this && myBoard[8] == this){
+            console.log(`${winner} wins`)
+            winningState = winner
+        }
+        if(myBoard[0] == this && myBoard[3] == this && myBoard[6] == this){
+            console.log(`${winner} wins`)
+            winningState = winner
+        }
+        if(myBoard[1] == this && myBoard[4] == this && myBoard[7] == this){
+            console.log(`${winner} wins`)
+            winningState = winner
+        }
+        if(myBoard[2] == this && myBoard[5] == this && myBoard[8] == this){
+            console.log(`${winner} wins`)
+            winningState = true
+        }
+        if(myBoard[3] == this && myBoard[5] == this && myBoard[8] == this){
+            console.log(`${winner} wins`)
+            winningState = winner;
+        }
+        if(myBoard[0] == this && myBoard[4] == this && myBoard[8] == this){
+            console.log(`${winner} wins`)
+            winningState = winner;
+        }
+        if(myBoard[2] == this && myBoard[4] == this && myBoard[6] == this){
+            console.log(`${winner} wins`)
+            winningState = winner;
+        }
+        if(totalMoves == 9 && winningState == ''){
+            winningState = "Tie"
+        }
+    }
+   
     spots.forEach(spot => spot.addEventListener('click', findIndex))
     spots.forEach(spot => spot.addEventListener('click', populateArray))    
-    return {populateArray}
+    return {populateArray, winningBoard}
 })()
 
 
